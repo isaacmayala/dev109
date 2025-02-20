c4468af2360beb7b78d69a9505cb23bcce39454e
Isaac Mayala <86272952+isaacmayala@users.noreply.github.com>
2025-02-19 16:48:50 -0800

    Update multiple-objects.js

--- m07/js/multiple-objects.js
+++ m07/js/multiple-objects.js
@@ -1,1 +1,30 @@
+// JavaScript & jQuery - Chapter 3: Functions, Methods & Objects
 
+// Hotel object constructor
+function Hotel(name, rooms, booked) {
+    this.name = name;
+    this.rooms = rooms;
+    this.booked = booked;
+    this.checkAvailability = function() {
+        return this.rooms - this.booked;
+    };
+    this.getDetails = function() { // New method to get formatted details
+      return this.name + ' rooms: ' + this.checkAvailability();
+    };
+}
+
+// Create hotel objects
+var quayHotel = new Hotel('Quay', 40, 25);
+var parkHotel = new Hotel('Park', 120, 77);
+var sunsetHotel = new Hotel('Sunset', 86, 10); // New hotel object
+
+// Update the HTML - using the new getDetails method
+document.getElementById('hotel1').textContent = quayHotel.getDetails();
+document.getElementById('hotel2').textContent = parkHotel.getDetails();
+document.getElementById('hotel3').textContent = sunsetHotel.getDetails(); // Display Sunset Hotel
+
+
+/*
+NOTE: textContent does not work in IE8 or earlier.
+You can use innerHTML, but be mindful of security issues (see p228-231 of the book).
+*/
